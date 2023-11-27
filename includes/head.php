8d3ee7e7d278c8fb9bc9ef5a91c3129847e32338
Edward Gilbert <egbot@asu.edu>
2023-11-27 14:00:35 -0700

    Force update of neon.css

--- includes/head.php
+++ includes/head.php
@@ -16,7 +16,7 @@ $CUSTOM_CSS_PATH = '/css/symb/custom';
 <link rel="stylesheet" href="<?php echo $CLIENT_ROOT; ?>/css/normalize.css">
 <link rel="stylesheet" href="<?php echo $CLIENT_ROOT; ?>/css/skeleton.css">
 <link rel="stylesheet" href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css">
-<link rel="stylesheet" href="<?php echo $CLIENT_ROOT; ?>/css/neon.css?ver=1">
+<link rel="stylesheet" href="<?php echo $CLIENT_ROOT; ?>/css/neon.css?ver=2">
 <link rel="stylesheet" href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=3" type="text/css">
 
 <script type="text/javascript" src="<?php echo $CLIENT_ROOT; ?>/js/symb/lang.js"></script>
