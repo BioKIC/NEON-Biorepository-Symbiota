4ac58c242267ed42d20f7b38b38db8f96db57a2c
Chandra Earl <sunray1@bellsouth.net>
2024-05-24 22:17:57 -0400

    Moved php content into react component

--- includes/head.php
+++ includes/head.php
@@ -12,9 +12,27 @@ $CUSTOM_CSS_PATH = '/css/symb/custom';
 ?>
 
 <!--neon react links-->
-<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/><meta name="theme-color" content="#000000"/><link rel="manifest" href="<?php echo $CLIENT_ROOT; ?>/neon-react/manifest.json"/><link rel="shortcut icon" href="<?php echo $CLIENT_ROOT; ?>/neon-react/favicon.ico?v=201912"/><link rel="preconnect" href="https://www.neonscience.org" crossorigin="anonymous"/><link rel="stylesheet" data-meta="drupal-fonts" href="<?php echo $CLIENT_ROOT; ?>/neon-react/assets/css/drupal-fonts.css"/><link rel="stylesheet" data-meta="drupal-theme" href="<?php echo $CLIENT_ROOT; ?>/neon-react/assets/css/drupal-theme.9632c20320a55418c76ed2e12456b01c.min.css"/><link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/><script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script><script>window.gtmDataLayer=[{page_category:"Core Components"}]</script><script>!function(e,t,a,n,g){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var m=t.getElementsByTagName(a)[0],r=t.createElement(a),s="&l="+n;r.async=!0,r.src="https://www.googletagmanager.com/gtm.js?id=GTM-K4S83R2"+s,m.parentNode.insertBefore(r,m)}(window,document,"script","gtmDataLayer")</script><script>window.NEON_SERVER_DATA="__NEON_SERVER_DATA__"</script><script defer="defer" src="<?php echo $CLIENT_ROOT; ?>/neon-react/static/js/main.js"></script><link href="<?php echo $CLIENT_ROOT; ?>/neon-react/static/css/main.css" rel="stylesheet">
-
+<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/><meta name="theme-color" content="#000000"/><link rel="manifest" href="<?php echo $CLIENT_ROOT; ?>/neon-react/manifest.json"/><link rel="shortcut icon" href="<?php echo $CLIENT_ROOT; ?>/neon-react/favicon.ico?v=201912"/><link rel="preconnect" href="https://www.neonscience.org" crossorigin="anonymous"/><link rel="stylesheet" data-meta="drupal-fonts" href="<?php echo $CLIENT_ROOT; ?>/neon-react/assets/css/drupal-fonts.css"/><link rel="stylesheet" data-meta="drupal-theme" href="<?php echo $CLIENT_ROOT; ?>/neon-react/assets/css/drupal-theme.9632c20320a55418c76ed2e12456b01c.min.css"/><link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/><script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script><script>window.gtmDataLayer=[{page_category:"Core Components"}]</script><script>!function(e,t,a,n,g){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var m=t.getElementsByTagName(a)[0],r=t.createElement(a),s="&l="+n;r.async=!0,r.src="https://www.googletagmanager.com/gtm.js?id=GTM-K4S83R2"+s,m.parentNode.insertBefore(r,m)}(window,document,"script","gtmDataLayer")</script><script>window.NEON_SERVER_DATA="__NEON_SERVER_DATA__"</script><script defer="defer" src="<?php echo $CLIENT_ROOT; ?>/neon-react/static/js/main.543f5428.js"></script><link href="<?php echo $CLIENT_ROOT; ?>/neon-react/static/css/main.8d445573.css" rel="stylesheet">
 <!--end of neon react links-->
+
+<!--To move innertext into neon-page.content-->
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+         // Get the div with id 'innertext'
+        var innerTextDiv = document.getElementById('innertext');
+        
+        // Get the div with data-selenium attribute 'neon-page.content'
+        var targetDiv = document.querySelector('div[data-selenium="neon-page.content"]');
+        
+        // Append the innerTextDiv as the last child of the targetDiv
+        if (innerTextDiv && targetDiv) {
+          targetDiv.appendChild(innerTextDiv);
+        }      
+    });
+ 
+</script>
+<!--end-->
+
 
 <link href="<?php echo $CLIENT_ROOT; ?>/css/v202209/jquery-ui.css" type="text/css" rel="stylesheet">
 <!-- UNIVERSAL CSS –––––––––––––––––––––––––––––––––––––––––––––––––– -->
