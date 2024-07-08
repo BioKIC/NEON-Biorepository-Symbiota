f84ec2484a87d7e94fdbc6be3e0f02eac55c0466
Chandra Earl <sunray1@bellsouth.net>
2024-07-08 15:43:31 -0400

    Hero title

--- index.php
+++ index.php
@@ -30,8 +30,10 @@ $statsArr = json_encode($stats->getBlueNeonStats());
 </head>
 
 <script>
+// image resizings
 	document.addEventListener('DOMContentLoaded', function() {
-		function updateElementWidth() {
+		function updateElementWidth() {	
+			// blue div
 			var neonPageContent = document.querySelector('div[data-selenium="neon-page.content"]');
 			var neonPageContentWidth = neonPageContent.offsetWidth;
 
@@ -80,29 +82,9 @@ $statsArr = json_encode($stats->getBlueNeonStats());
 </script>
 
 <body class="home-page">
-	<style>
-		.bar:hover {
-			opacity: 0.5;
-		}
-
-		.bar-label {
-			font-size: 7px !important;
-			color: #0073cf !important;
-		}
-
-		.bar-label a {
-			color: #0073cf !important;
-			text-decoration: underline;
-		}
-	</style>
 	<div id="biorepo-page"></div>
 	<!-- This is inner text! -->
 	<div id="innertext">
-		<section style="margin-bottom: 3em;">
-			<div style="border-bottom-width:2px;border-color:#F0AB00;border-left-width:20px;border-right-width:2px;border-style:solid;border-top-width:2px;padding:10px;">
-				<p>A new <a href="https://www.nsf.gov/pubs/2024/nsf24069/nsf24069.jsp?WT.mc_ev=click&amp;WT.mc_id=&amp;utm_medium=email&amp;utm_source=govdelivery">NSF DCL presents an opportunity to leverage the NEON Biorepository collections</a>. Please <a href="mailto:biorepo@asu.edu">contact us</a> with any questions or for information needed for potential innovative use! We are here to support you.</p>
-			</div>
-		</section>
 		<div id="biorepo-home-page-content"></div>
 	</div>
 </body>
