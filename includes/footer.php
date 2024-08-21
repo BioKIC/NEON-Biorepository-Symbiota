7a039727df813df081fb33e5e45487f67b77efa2
Chandra Earl <sunray1@bellsouth.net>
2024-08-21 12:20:49 -0400

    Updated NSF name

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
 						<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsfNeon_blue_2023.png"  width="150px" alt="NSF NEON | Open Data to Understand our Ecosystems">
 					</a>
 					<span style="color: gray; font-size: 12px;line-height:1.4em">
-						<i>The National Ecological Observatory Network is a major facility fully funded by the National Science Foundation. Any opinions, findings and conclusions or recommendations expressed in this material do not necessarily reflect the views of the National Science Foundation.</i>
+						<i>The National Ecological Observatory Network is a major facility fully funded by the U.S. National Science Foundation. Any opinions, findings and conclusions or recommendations expressed in this material do not necessarily reflect the views of the U.S. National Science Foundation.</i>
 					</span>
 				</div>
 				<div class="footer-bottom-right">
