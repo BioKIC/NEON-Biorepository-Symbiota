a1fc32692522e732bd417f20803d43dfc561d55e
Chandra Earl <sunray1@bellsouth.net>
2024-09-10 17:55:01 -0400

    Fixed deprecated sanitization

--- collections/editor/rpc/getnewdetitem.php
+++ collections/editor/rpc/getnewdetitem.php
@@ -3,10 +3,10 @@ include_once('../../../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/OccurrenceEditorDeterminations.php');
 
 $collid = array_key_exists('collid' ,$_REQUEST) ? filter_var($_REQUEST['collid'], FILTER_SANITIZE_NUMBER_INT) : '';
-$catNum = array_key_exists('catalognumber', $_REQUEST) ? filter_var($_REQUEST['catalognumber'], FILTER_SANITIZE_STRING) : '';
+$catNum = array_key_exists('catalognumber', $_REQUEST) ? htmlspecialchars($_REQUEST['catalognumber'], ENT_QUOTES, 'UTF-8') : '';
 $allCatNum = array_key_exists('allcatnum', $_REQUEST) ? filter_var($_REQUEST['allcatnum'], FILTER_SANITIZE_NUMBER_INT) : 0;
-$sciName = array_key_exists('sciname', $_REQUEST) ? filter_var($_REQUEST['sciname'], FILTER_SANITIZE_STRING) : '';
-$fieldSite = array_key_exists('fieldsite', $_REQUEST) ? filter_var($_REQUEST['fieldsite'], FILTER_SANITIZE_STRING) : 0;
+$sciName = array_key_exists('sciname', $_REQUEST) ? htmlspecialchars($_REQUEST['sciname'], ENT_QUOTES, 'UTF-8') : '';
+$fieldSite = array_key_exists('fieldsite', $_REQUEST) ? filter_var($_REQUEST['fieldsite'], FILTER_SANITIZE_NUMBER_INT) : 0;
 
 $retArr = array();
 if($collid){
