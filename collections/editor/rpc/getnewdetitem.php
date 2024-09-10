36531250b6ac1977b92f070c3d2487003852989e
Chandra Earl <sunray1@bellsouth.net>
2024-09-10 16:45:07 -0400

    Added geographical option to batch dets

--- collections/editor/rpc/getnewdetitem.php
+++ collections/editor/rpc/getnewdetitem.php
@@ -6,12 +6,13 @@ $collid = array_key_exists('collid' ,$_REQUEST) ? filter_var($_REQUEST['collid']
 $catNum = array_key_exists('catalognumber', $_REQUEST) ? filter_var($_REQUEST['catalognumber'], FILTER_SANITIZE_STRING) : '';
 $allCatNum = array_key_exists('allcatnum', $_REQUEST) ? filter_var($_REQUEST['allcatnum'], FILTER_SANITIZE_NUMBER_INT) : 0;
 $sciName = array_key_exists('sciname', $_REQUEST) ? filter_var($_REQUEST['sciname'], FILTER_SANITIZE_STRING) : '';
+$fieldSite = array_key_exists('fieldsite', $_REQUEST) ? filter_var($_REQUEST['fieldsite'], FILTER_SANITIZE_STRING) : 0;
 
 $retArr = array();
 if($collid){
 	$occManager = new OccurrenceEditorDeterminations();
 	$occManager->setCollId($collid);
-	$retArr = $occManager->getNewDetItem($catNum,$sciName,$allCatNum);
+	$retArr = $occManager->getNewDetItem($catNum,$sciName,$allCatNum, $fieldSite);
 }
 
 echo json_encode($retArr);
