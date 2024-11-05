ec1ecdbef516827a0351b163a008bc93d56a834e
Chandra Earl <sunray1@bellsouth.net>
2024-11-05 16:01:28 -0500

    testing breadcrumbs

--- includes/head.php
+++ includes/head.php
@@ -30,12 +30,7 @@ elseif (array_key_exists('CollAdmin', $USER_RIGHTS) || array_key_exists('CollEdi
         var innerText = document.getElementById("innertext");
         if (innerText) {
             innerText.parentNode.insertBefore(biorepoPage, innerText);
-        }
-        
-        const breadcrumbLink = document.querySelector('nav a[href="https://biokic4.rc.asu.edu/neon/portal/misc/neoncollprofiles.php?collid=#"]');
-        if (breadcrumbLink) {
-            breadcrumbLink.href = breadcrumbLink.href.replace('#', '<?php echo $collid; ?>');
-        }  
+        } 
         
         //javascript code created by React
         var reactScript = document.createElement('script');
@@ -348,6 +343,13 @@ elseif (array_key_exists('CollAdmin', $USER_RIGHTS) || array_key_exists('CollEdi
     document.body.appendChild(reactScript);
    
     });
+    
+    window.onload = function () {
+        const breadcrumbLink = document.querySelector('nav a[href="https://biokic4.rc.asu.edu/neon/portal/misc/neoncollprofiles.php?collid=#"]');
+        if (breadcrumbLink) {
+            breadcrumbLink.href = breadcrumbLink.href.replace('#', '<?php echo $collid; ?>');
+        }
+    };
 </script>
 <!--end-->
 
